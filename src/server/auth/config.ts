a31953a61cbbae1494359cee55cfbad3ebd35321
Alvin He <alvin@Alvins-MacBook-Pro.local>
2025-11-10 18:11:06 +1100

    changed config.ts google provider secret and id to match .env

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -15,8 +15,8 @@ declare module "next-auth" {
 export const authConfig = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
       authorization: {
         params: {
           prompt: "select_account",
