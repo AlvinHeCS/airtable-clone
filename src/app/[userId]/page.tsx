b73e289eb6481ddfe0e36adc6656af760e860d8f
Alvin He <alvin@Alvins-MacBook-Pro.local>
2025-11-11 17:21:06 +1100

    fixed bug

--- src/app/[userId]/page.tsx
+++ src/app/[userId]/page.tsx
@@ -13,7 +13,13 @@ export default function UserIdPage() {
             utils.user.getBases.invalidate();
         }
     })
-
+    const handleCreateBase = async () => {
+        try {
+          await mutateAsync();
+        } catch (err) {
+          console.error(err);
+        }
+      };
     if (isLoading) {
         return (
             <>
@@ -35,19 +41,7 @@ export default function UserIdPage() {
                     )
                 })}
             </div>
-            <button
-  onClick={() => {
-    void (async () => {
-      try {
-        await mutateAsync();
-      } catch (err) {
-        console.error(err);
-      }
-    })();
-  }}
->
-  create bases
-</button>
+            <button onClick={() => void handleCreateBase()}>create bases</button>
         </>
     )
 };
