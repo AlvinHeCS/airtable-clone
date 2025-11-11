64c8cf72f1e91ac0507ceaeb5efbda1eaf5ebb6d
Alvin He <alvin@Alvins-MacBook-Pro.local>
2025-11-11 14:21:49 +1100

    added a check for users

--- src/app/_components/loginButton.tsx
+++ src/app/_components/loginButton.tsx
@@ -3,13 +3,14 @@
 import { signIn, useSession } from "next-auth/react"
 import { useRouter } from "next/navigation";
 import { useEffect } from "react"
-
+import { api } from "~/trpc/react";
 
 export default function LoginButton() {
     const router = useRouter();
     const session = useSession();
-
+    const { data, isLoading } = api.user.getUsers.useQuery();
     useEffect(() => {
+        console.log("this is users data", data);
         console.log("status:", session.status);
         console.log("data:", session.data);
         if (session.data?.user) {
---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+    getUsers: publicProcedure.query(async ({ ctx }) => {
+        return await ctx.db.user.findMany()
+    })
+
+})
